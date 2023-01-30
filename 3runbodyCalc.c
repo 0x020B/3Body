ae5ac4646f4ab7cdb5af97caeb99b9f757c88f35
n0tr00t <106423923+n0tr00teuorg@users.noreply.github.com>
2023-01-30 07:59:04 +0000

    Create 3runbodyCalc.c

---
+++ 3runbodyCalc.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void msg(char *s){
+    puts(s);
+    usleep(9000);
+}
+
+int main(void)
+{
+    int j;
+    msg("***********************************");
+    msg("   \e[32m[INFO]\e[0m   用户ID : weicheng");
+    msg("   \e[33m[SYS]\e[0m    计算任务已经提交");
+    msg("   \e[32m[INFO]\e[0m   任务ID : 89345786");
+    msg("   \e[33m[SYS]\e[0m    申请节点 : node00001-node08192");
+    msg("   \e[33m[SYS]\e[0m    优先级 : 最高\n");
+    msg("\e[34m[STATUS]\e[0m    等待计算资源\n");
+    msg("   \e[33m[SYS]\e[0m    资源申请成功");
+    msg("   \e[33m[SYS]\e[0m    初始化计算任务.....");
+    msg("\e[34m[STATUS]\e[0m    计算已启动\n");
+    msg("------------------------");
+    msg("  3bodyCalc v 3.12.83");
+    for (int i = 0; i<=100; i++){
+        printf("  Proc:: ");
+        for (j = 0; j <= i; j++)
+            printf("%s", "\u2588");
+        for (; j<=100; j++)
+            printf(" ");
+        printf(" %d%%\r", i);
+        fflush(stdout);
+        usleep(5000);
+    }
+    msg("\n------------------------");
+    msg("   \e[33m[SYS]\e[0m    计算已完成");
+}
